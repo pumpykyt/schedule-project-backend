2a191279396e5d5f16c61997f183c5ce6f888efb
veremeychukden <veremeychukden@gmail.com>
2022-05-17 12:19:09 +0300

    Add validator for ScheduleCreateService

---
+++ ScheduleManager.Web/Validators/ScheduleValidator.cs
@@ -0,0 +1,16 @@
+﻿using FastEndpoints;
+using FluentValidation;
+using ScheduleManager.Contracts.Requests;
+
+namespace ScheduleManager.Web.Validators;
+
+public class ScheduleValidator : Validator<ScheduleCreateRequest>
+{
+    public ScheduleValidator()
+    {
+        RuleFor(t => t.Name)
+            .NotNull().WithMessage("Name is null")
+            .NotEmpty().WithMessage("Name is empty")
+            .Length(1, 250);
+    }       
+}

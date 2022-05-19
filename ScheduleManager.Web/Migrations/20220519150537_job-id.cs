4a276febbd1bb80db2740d661805364a0366cf4c
aesense <senseandrey@gmail.com>
2022-05-19 20:10:46 +0300

    add job-id migration

---
+++ ScheduleManager.Web/Migrations/20220519150537_job-id.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScheduleManager.Web.Migrations
+{
+    public partial class jobid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "JobId",
+                table: "ScheduleEvents",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JobId",
+                table: "ScheduleEvents");
+        }
+    }
+}
